482f03006383a33fe99b745db725a7be63ad7722
Nirav Desai <Nirav.Desai@pentanasolutions.com>
2026-08-17 11:24:50 +1000

    Update program settings
    -Enforce JSON enum serialization as strings
    -Modified  launch profile

--- Backend/TODO.Backend.API/Program.cs
+++ Backend/TODO.Backend.API/Program.cs
@@ -1,10 +1,26 @@
+using Microsoft.AspNetCore.Http.Json;
 using Scalar.AspNetCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// 1. Force JSON serialization to use strings for enums
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+// For Controller-based APIs, apply it to AddControllers as well
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
